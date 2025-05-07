a5d0af013e1293b0a062d0de082432f2bd4cc68f
richasinghkk <146985975+richasinghkk@users.noreply.github.com>
2025-05-07 11:45:32 +0530

    update

--- react-portfolio-main/src/components/Hero/Hero.jsx
+++ react-portfolio-main/src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/richasingh.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
